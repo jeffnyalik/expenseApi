9e8f754d607f5411dd3047d524970d31a7424822
Jeff Nyalik <jeffnyak@gmail.com>
2021-04-30 17:12:10 +0300

    Email configuration

--- core/settings.py
+++ core/settings.py
@@ -39,11 +39,20 @@ SIMPLE_JWT = {
 }
 
 ## Email configuration
+# EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST=config('EMAIL_HOST')
+# EMAIL_PORT=config('EMAIL_PORT')
+# EMAIL_HOST_USER=config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = True
+## End
+
+## Email configuration
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST=config('EMAIL_HOST')
-EMAIL_PORT=config('EMAIL_PORT')
-EMAIL_HOST_USER=config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST=''
+EMAIL_PORT=587
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS = True
 ## End
 
