58bd28ee2f5c4d47637aacd4fda903da2b5fb453
Jeff Nyalik <jeffnyak@gmail.com>
2021-05-04 12:27:19 +0300

    updates

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -14,3 +14,10 @@ EMAIL_HOST_USER=''
 EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS = True
 ## End
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
