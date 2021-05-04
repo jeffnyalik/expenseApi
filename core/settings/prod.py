d4e6a060502d9e7ea29fde89f5865b33bc185b75
Jeff Nyalik <jeffnyak@gmail.com>
2021-05-04 12:34:19 +0300

    updates

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,16 +1,16 @@
-from core.settings.base import *
-from decouple import config
-import os
+# from core.settings.base import *
+# from decouple import config
+# import os
 
-SECRET_KEY = 'dfdhfjhdjfkjk3834'
-DEBUG = False
+# SECRET_KEY = 'dfdhfjhdjfkjk3834'
+# DEBUG = False
 
 
-## Email configuration
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST=''
-EMAIL_PORT=''
-EMAIL_HOST_USER=''
-EMAIL_HOST_PASSWORD=''
-EMAIL_USE_TLS = True
-## End
+# ## Email configuration
+# EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST=''
+# EMAIL_PORT=''
+# EMAIL_HOST_USER=''
+# EMAIL_HOST_PASSWORD=''
+# EMAIL_USE_TLS = True
+# ## End
